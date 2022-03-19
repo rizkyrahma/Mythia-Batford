9e249bab54865c88f26dfe43e4f70a0a5f65980c
Nur Alam <99713260+rizkyrahma@users.noreply.github.com>
2022-03-19 13:38:29 +0700

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, participants }) => {
 conn.reply(m.chat, `Iya Kak? ${conn.getName(conn.user.jid)} Online Ketik Aja *#menu* Untuk Melihat List Menu *${conn.getName(conn.user.jid)}* :)\n\nSilahkan Ketik #menu`, 'conversation', { quoted: m, contextInfo: { externalAdReply :{
-mediaUrl: 'https://wa.me/62895603352610?text=Hai.com',
+mediaUrl: 'https://wa.me/6285888083107?text=Hai.com',
 mediaType: 2,
 title: `Simple Bot Whatsapp`,
 body: '© YUKIBOT',
